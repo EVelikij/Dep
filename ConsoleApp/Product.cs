7e796d956e5455127f1b4071e1473f2dfb323104
evelikij <evelikij@gmail.com>
2018-06-05 16:34:09 +0300

    initial commit

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Product : IProduct
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProduct prod = new Product();
+            prod.Name = "Test";
+
+            Console.WriteLine(prod.Name.ToString());
+            Console.ReadKey();
+        }
+    }
+}
